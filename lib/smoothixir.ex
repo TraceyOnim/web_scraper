efc77e7e83e6624057d17bbaa2fddef2bb8c5a71
Tee22 <traceyOnim22@gmail.com>
2020-07-12 13:23:37 +0300

    initial commit

---
+++ lib/smoothixir.ex
@@ -0,0 +1,18 @@
+defmodule Smoothixir do
+  @moduledoc """
+  Documentation for `Smoothixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Smoothixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
